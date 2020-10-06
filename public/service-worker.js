0c3992037bdaceabed6a1fe65712c023b5d1ec7f
Blake Arambula <bfire23@gmail.com>
2020-10-05 20:33:51 -0700

    starting service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,12 @@
-const FILES_TO_CACHE = ["/", "index.html", "service-worker.js", "index.js",
-"styles.css",];
+const FILES_TO_CACHE = [
+"/",
+"index.html",
+"service-worker.js",
+"index.js",
+"styles.css",
+"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+"https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+
+];
+
 
