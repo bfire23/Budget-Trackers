6c56b3a5b087dad6979d0bcc200a648df615242d
Blake Arambula <bfire23@gmail.com>
2020-10-09 13:53:55 -0700

    setting up db.js

--- public/db.js
+++ public/db.js
@@ -1,9 +1,10 @@
 let db;
 
+
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
-    const db = event.target.results;
+    let db = event.target.results;
     db.createObjectStore("pending", { autoIncrement: true});
 };
 
@@ -25,4 +26,41 @@ function saveRecord(record) {
     const store = transaction.objectStore("pending");
 
     store.add(record);
+
+    function checkDatabase() {
+        // open a transaction on your pending db
+        const transaction = db.transaction(["pending"], "readwrite");
+        // access your pending object store
+        const store = transaction.objectStore("pending");
+        // get all records from store and set to a variable
+        const getAll = store.getAll();
+      
+        getAll.onsuccess = function() {
+          if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+              method: "POST",
+              body: JSON.stringify(getAll.result),
+              headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+              }
+            })
+            .then(response => response.json())
+            .then(() => {
+              // if successful, open a transaction on your pending db
+              const transaction = db.transaction(["pending"], "readwrite");
+      
+              // access your pending object store
+              const store = transaction.objectStore("pending");
+      
+              // clear all items in your store
+              store.clear();
+            });
+          }
+        };
+      }
+      
+      // listen for app coming back online
+      window.addEventListener("online", checkDatabase);
+      
 }
--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
 "/service-worker.js",
 "/index.js",
 "/styles.css",
+"/db.js",
 "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 "/manifest.webmanifest.js",
