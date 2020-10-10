58965fbcd9de2bc60588753d417b2e83aee5b592
Blake Arambula <bfire23@gmail.com>
2020-10-10 10:04:02 -0700

    fixed service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,11 +5,11 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/styles.css",
   "/db.js",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
-  "/manifest.webmanifest",
-  "icons/icon-192x192.png",
-  "icons/icon-512x152.png"
+  // "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  // "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 
 ];
 
